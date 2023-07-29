799bf97934128d32aac1526cc80ffcd9b200b32c
dcbrian <ivucady@gmail.com>
2023-07-30 04:32:44 +0900

    fix mobile likebutton overlay

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -95,7 +95,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 h-full">
       <div className="flex w-full justify-start">
-        <div className="flex items-center gap-x-4">
+        <div className="flex items-center gap-x-4 z-0">
           <MediaItem data={song} />
           <LikeButton songId={song.id} />
         </div>
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -73,7 +73,7 @@ const Sidebar = ({ children, songs }: SidebarProps) => {
           <Library songs={songs} />
         </Box>
       </div>
-      <main className="h-full flex-1 overflow-y-auto py-2 pr-2">{children}</main>
+      <main className="h-full flex-1 overflow-y-auto py-2 md:pr-2">{children}</main>
     </div>
   );
 };
